6812af355b58d5855be648a73d7867a93d15681d
Claire Armistead <claire.armistead@noaa.gov>
2016-03-03 09:10:51 -0900

    added comments

--- rachel.sh
+++ rachel.sh
@@ -1,6 +1,9 @@
+#another comment
 # calculate stats
 for datafile in "$@"
 do
-	echo $(datafile)
+	echo ${datafile}
 	bash goostats -J 100 -r $datafile stats-$datafile
 done
+#example of versioning
+
